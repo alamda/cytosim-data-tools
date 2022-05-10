fixed axial force calculation to dot product and added some documentation

fil_axial_forces.py
@@ -6,71 +6,75 @@ class FilAxialForces(Data):
 	def __init__(self, column_list):
 		super(). __init__(column_list=column_list)
 
-		self.fil_force_df = pd.DataFrame(columns=[ 'fil_id', 'fx', 'fy' ])
+		self.fil_force_df = pd.DataFrame(columns=[ 'fil_id', 'f' ])
 		self.sum_output_df = pd.DataFrame()
 
 	def calc_force_vec_proj(self, fil_vec, force_vec):
 
-		return fil_vec * np.dot(fil_vec, force_vec) / np.dot(fil_vec, fil_vec)
+		# Maybe these vectors need to be normalized first???
+		# fil_vec is taken directly from cytosim data and is normalized
+		# I have not checked that force_vec is normalized (it probably isn't, now that i think about it.)
+		return np.dot(fil_vec, force_vec)
 
 	def calc_fil_forces(self):
 
 		for couple_id, df_couple in self.temp_dataframe.groupby('identity'):
+			# Get direction vectors of of forces exerted by motor hands
 			cpl_dir1 = np.array( [ df_couple['pos2X'] - df_couple['pos1X'] , \
 							  df_couple['pos2Y'] - df_couple['pos1Y'] ] ).flatten()
 
 			cpl_dir2 = np.array( [ df_couple['pos1X'] - df_couple['pos2X'] , \
 							  df_couple['pos1Y'] - df_couple['pos2Y'] ] ).flatten()
 
+			# Get direction vector of filaments
 			fil_dir1 = np.array( [ df_couple['dirFiber1X'], df_couple['dirFiber1Y'] ] ).flatten()
 			fil_dir2 = np.array( [ df_couple['dirFiber2X'], df_couple['dirFiber2Y'] ] ).flatten()
 
+			# Get the magnitude of the force
 			force_mag = df_couple['force'].values[0]
 
+			# Get indexes of motor and filaments
 			cluster_id = df_couple['cluster'].values[0]
 			fil_id1 = df_couple['fiber1'].values[0]
 			fil_id2 = df_couple['fiber2'].values[0]
 
 			if self.args.largest:
 				if cluster_id == self.largest_cluster_id:
-					f_vec1 = force_mag * self.calc_force_vec_proj(fil_dir1, cpl_dir1)
-					f_vec2 = force_mag * self.calc_force_vec_proj(fil_dir2, cpl_dir2)
+					# Need to multiply force magnitude bc direction vectors should be (?) unit vectors
+					f1 = force_mag * self.calc_force_vec_proj(fil_dir1, cpl_dir1)
+					f2 = force_mag * self.calc_force_vec_proj(fil_dir2, cpl_dir2)
 
-					new_df1 = pd.DataFrame( [[ fil_id1, f_vec1[0], f_vec1[1] ]],\
-											columns = [ "fil_id", "fx", "fy" ])
+					new_df1 = pd.DataFrame( [[ fil_id1, f1 ]],\
+											columns = [ "fil_id", "f" ])
 
 
-					new_df2 = pd.DataFrame( [[ fil_id2, f_vec2[0], f_vec2[1] ]],\
-											columns = [ "fil_id", "fx", "fy" ])
+					new_df2 = pd.DataFrame( [[ fil_id2, f2 ]],\
+											columns = [ "fil_id", "f" ])
 			else:
-				f_vec1 = force_mag * self.calc_force_vec_proj(fil_dir1, cpl_dir1)
-				f_vec2 = force_mag * self.calc_force_vec_proj(fil_dir2, cpl_dir2)
+				f1 = force_mag * self.calc_force_vec_proj(fil_dir1, cpl_dir1)
+				f2 = force_mag * self.calc_force_vec_proj(fil_dir2, cpl_dir2)
 
-				new_df1 = pd.DataFrame( [[ fil_id1, f_vec1[0], f_vec1[1] ]],\
-										columns = [ "fil_id", "fx", "fy" ])
-
-
-				new_df2 = pd.DataFrame( [[ fil_id2, f_vec2[0], f_vec2[1] ]],\
-										columns = [ "fil_id", "fx", "fy" ])
+				new_df1 = pd.DataFrame( [[ fil_id1, f1 ]],\
+										columns = [ "fil_id", "f" ])
 
 
-
+				new_df2 = pd.DataFrame( [[ fil_id2, f2 ]],\
+										columns = [ "fil_id", "f" ])
 
 			self.fil_force_df = pd.concat([self.fil_force_df, new_df1], ignore_index=True)
 			self.fil_force_df = pd.concat([self.fil_force_df, new_df2], ignore_index=True)
 
+		# find the total force along each filament
 		for fil_id, fil_df in self.fil_force_df.groupby("fil_id"):
-			fx_sum = fil_df['fx'].sum()
-			fy_sum = fil_df['fy'].sum()
+			f_sum = fil_df['f'].sum()
 
-			new_df = pd.DataFrame( [[ fil_id, fx_sum, fy_sum ]], columns=[ "fil_id", "fx_sum", "fy_sum" ])
+			new_df = pd.DataFrame( [[ fil_id, f_sum ]], columns=[ "fil_id", "f_sum" ])
 
 			self.output_df = pd.concat([self.output_df, new_df], ignore_index=True)
 
-		fx_sum = self.output_df["fx_sum"].sum()
-		fy_sum = self.output_df["fy_sum"].sum()
+		f_sum = self.output_df["f_sum"].sum()
 
-		self.sum_output_df = pd.DataFrame( [[ fx_sum, fy_sum ]], columns=["fx_sum", "fy_sum"])
+		self.sum_output_df = pd.DataFrame( [[ f_sum ]], columns=["f_sum"])
 
 	def analyze_forces(self):
 		self.calc_fil_forces()